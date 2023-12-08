18181c8b0297c20139501389a7cb2ac0f8f07328
Masanori Ogino <mogino@acm.org>
2023-12-08 12:18:22 +0900

    KNF
    Formatter: knfmt 4.2.0
    Signed-off-by: Masanori Ogino <mogino@acm.org>

--- main.c
+++ main.c
@@ -46,14 +46,14 @@
 #include "Const.h"
 #include "server.h"
 
-extern	int	fork_flag;
-extern	char	*lock_file;
+extern int	 fork_flag;
+extern char	*lock_file;
 
 char	*version_number = "2.08C";
 char	*time_stamp = "Mon Mar 23 16:42:59 JST 1998";
 
-static	void
-opening (void)
+static void
+opening(void)
 {
 	printf("Kana-Kanji Conversion Server Version %s\r\n", version_number);
 	printf("Copyright (c) 1990-1995 Sony Corporation\r\n");
@@ -62,34 +62,33 @@ opening (void)
 
 #ifdef	LOCK_FILE
 
-
 int
-make_lockfile (void)
+make_lockfile(void)
 {
 	int	fd;
 
 	fd = open(lock_file, O_CREAT | O_EXCL, 0600);
-	if (fd < 0) return ERROR;
+	if (fd < 0)
+		return ERROR;
 	close(fd);
 	return 0;
 }
 
-
 int
-erase_lockfile (void)
+erase_lockfile(void)
 {
 	return unlink(lock_file);
 }
 #endif
 
 static int
-signal_handler (int sig)
+signal_handler(int sig)
 {
 	warning_out("signal %d catched.", sig);
 }
 
-static	int
-terminate_handler (int sig)
+static int
+terminate_handler(int sig)
 {
 	close_socket();
 	sj_closeall();
@@ -100,7 +99,7 @@ terminate_handler (int sig)
 }
 
 void
-server_terminate (void)
+server_terminate(void)
 {
 	close_socket();
 	sj_closeall();
@@ -110,8 +109,8 @@ server_terminate (void)
 	exit(0);
 }
 
-static	void
-exec_fork (void)
+static void
+exec_fork(void)
 {
 	int	tmp;
 
@@ -119,11 +118,10 @@ exec_fork (void)
 		if (tmp < 0) {
 			fprintf(stderr, "Can't fork child process\r\n");
 			fflush(stderr);
-		}
-		else {
+		} else {
 			signal(SIGCHLD, _exit);
-			signal(SIGHUP , SIG_IGN);
-			signal(SIGINT , SIG_IGN);
+			signal(SIGHUP, SIG_IGN);
+			signal(SIGINT, SIG_IGN);
 			signal(SIGQUIT, SIG_IGN);
 			signal(SIGTSTP, SIG_IGN);
 			signal(SIGTERM, _exit);
@@ -132,30 +130,28 @@ exec_fork (void)
 		}
 	}
 
-	signal(SIGHUP,  (void (*)())signal_handler);
+	signal(SIGHUP, (void (*)())signal_handler);
 
-
-	signal(SIGINT,  (void (*)())terminate_handler);
+	signal(SIGINT, (void (*)())terminate_handler);
 	signal(SIGQUIT, (void (*)())signal_handler);
 	signal(SIGTERM, (void (*)())terminate_handler);
 	if (fork_flag)
 		signal(SIGTSTP, SIG_IGN);
 }
 
-
-
-static	void
-leave_tty (void)
+static void
+leave_tty(void)
 {
 	int	tmp;
 
-	tmp  = open_error();
+	tmp = open_error();
 	tmp |= open_log();
 	tmp |= open_debug();
 	kill(getppid(), SIGTERM);
 	fclose(stdin);
 	fclose(stdout);
-	if (!tmp) fclose(stderr);
+	if (!tmp)
+		fclose(stderr);
 
 	if (fork_flag) {
 		if ((tmp = open("/dev/tty", O_RDWR)) >= 0) {
@@ -166,34 +162,34 @@ leave_tty (void)
 	}
 }
 
-
-
 int
-main (int argc, char **argv)
+main(int argc, char **argv)
 {
 	opening();
 #ifndef __NetBSD__
 	if (setuid(geteuid())) {
-		fprintf(stderr, "error at setuid.\r\n"); exit(1);
+		fprintf(stderr, "error at setuid.\r\n");
+		exit(1);
 	}
 #endif
 
-	parse_arg(argc, argv);		
-	read_runcmd();			
-	set_default();			
+	parse_arg(argc, argv);
+	read_runcmd();
+	set_default();
 
 #ifdef	LOCK_FILE
 	if (make_lockfile() == ERROR) {
-		fprintf(stderr, "Already running...\r\n"); exit(1);
+		fprintf(stderr, "Already running...\r\n");
+		exit(1);
 	}
 #endif
 
-	exec_fork();  
+	exec_fork();
 
 	socket_init();
 	open_socket();
 
-        leave_tty(); 
+	leave_tty();
 
 	preload_dict();
 	preopen_dict();
